22ada8e713c2618088ab366fb6580ecd20a8b178
Michael Hansen <michaelandhansen@gmail.com>
2020-09-06 16:18:18 -0400

    fixed portfolio import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SignUpForm from "./components/SignUpForm/SignUpForm";
 import LogInForm from "./components/LogInForm/LogInForm";
 import LogOut from "./components/LogOut/LogOut";
 import Home from "./components/Home/Home";
-import Portfolio from "./components/Portfolio";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
     return (
