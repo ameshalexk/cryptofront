908c840328d15dd05f038903a080a6e5ba543d17
Chris Howard <choward@ownerfinancellc.com>
2020-09-08 12:49:01 -0400

    hero component

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Hero.css";
+
+// import logo from './components/Home/Hero/logo.jpg';
 
 const Hero = () => {
-    return <div></div>;
+    return (
+    <div className="hero-container">
+    
+    <div className="hero-text">
+        <h1 className="hero-name">CoinBox</h1>
+        <h1 className="hero-h1">Manage Your Cryptocurrency Portfolio</h1>
+        <h1 className="hero-h2">CoinBox is a simple way to monitor market activity, make purchases, and review your profits and losses. Login or create account</h1>
+        </div>
+       
+        <div image-container>
+        <img className="hero-image" src="https://i.ibb.co/2MwbH9d/Bitcoins-hepa-Golden-virtual-currency-coins-background-3-D-Illustration.jpg"
+        alt="new"
+        />
+         </div>
+        
+
+    </div>
+    );
 };
 
 export default Hero;
